fdf169de628be03d0f22b5c35a907fc959b540aa
Brett Stewart <mygooglyeyes@users.noreply.github.com>
2015-01-14 19:20:59 -0800

    fixed non standard characters
    fixed the issue with twitter sending a control character after the tweet ended, resulting in full squares trailing the end of a tweet.

--- Arduino/LCD/tw2LCD.ino
+++ Arduino/LCD/tw2LCD.ino
@@ -4,62 +4,61 @@
 #include <Adafruit_RGBLCDShield.h>
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 #define WHITE 0x7
-char data[256]; //allow as many values as the Serial buffer will accept
-int idx = 0;
+char data; //allow as many values as the Serial buffer will accept
+char *idx;
+int loops = 0;
 int wait = 150;  //control the speed of that the text sent over Serial is printed
 
 void setup()
 {
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.setCursor(16, 0);
+  lcd.begin(16, 2);  // Initalize the LCD
+  lcd.clear();  // Clear the display
+  lcd.setCursor(16, 0);  //Set the cursor position
   //lcd.rightToLeft();
   //lcd.autoscroll();
-  Serial.begin(115200);
+  Serial.begin(115200);  //Start the serial port and set it to 115200 Baud
 
 }
 
 void loop ()
 {
 
-  while (idx != 200) // this while statement will allow a 2 x 16 lcd display to print 32 characters, then set idx to 0, thus resetting the loop
+  lcd.clear();  //Clear the display
+  lcd.noAutoscroll();  //make sure autoscrolling is off
+  while (Serial.available())  //While there is data in the serial buffer
   {
-    lcd.clear();
-    lcd.noAutoscroll();
-    while (Serial.available())
+    delay(10);  //Wait a bit to make sure the data has time to transfer
+
+    if (Serial.available() > 0 )  //If there is data in the serial buffer
     {
-      delay(10);
-      
-      if (Serial.available() > 0 )
+      if (loops <= 16) // check that we are not at the end of the first line
       {
-        if (idx <= 16) // then check that we are not at the end of the second line
-        {
-
-          data[idx++] = Serial.read(); //if we are not, take the value from the serial and advance the array
-          lcd.print(data[idx - 1]); //print the value from whereever the cursor is
-          delay(wait);
-        }
-
-        else             //otherwise the value equals 32 and we are at the end of the second line
-        {
-
-
-          //lcd.setCursor(0,1);
-          lcd.autoscroll();
-          while (idx <= 39) // then check that we are not at the end of the second line
-          {
-            data[idx++] = Serial.read(); //if we are not, take the value from the serial and advance the array
-            lcd.print(data[idx - 1]); //print the value from whereever the cursor is
-            delay(wait);
-          }
-          idx = 0;
-          lcd.setCursor(0,0);
-
-        }
+        idx = &data;  //set the pointer idx to the address of the current character in 'data'
+        data = Serial.read(); // take the value from the serial buffer
+        lcd.print(*idx); //print the value from wherever the pointer is set to
+        loops++;  // advance the array
+        delay(wait);  //delay the period in 'wait'
       }
-    }
-    lcd.clear();
-    idx = 0;
-  }
-}
 
+      else             //otherwise the value equals 17 and we are at the end of the first line
+      {
+        lcd.autoscroll();  //turn on autoscroll so the characters move ticker style left to right
+        do 
+        {
+          idx = &data;  //set the pointer idx to the address of the current character in 'data'
+          data = Serial.read(); //take the value from the serial buffer
+          lcd.print(*idx); //print the value from wherever the pointer is set to
+          loops++;  //advance the array
+          delay(wait); //delay the period in 'wait'
+        } while (loops <=39 && data >31 && data <128); // then check that the LCD's buffer is not 
+                                                       //more than 39 (the max for one line) or 
+                                                       //anything but standard characters
+        loops = 0;  //reset 'loops' to 0
+        lcd.setCursor(0,0);  //move the cursor back to 0,0
+
+      }  
+    }
+  }
+  lcd.clear();  //clear the display
+  loops = 0;  //reset 'loops' to 0
+}
